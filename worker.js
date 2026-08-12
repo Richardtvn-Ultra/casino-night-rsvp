7f4a3e0dfb0a4a339d7790eecb4b8d7b8573ee3d
Richardtvn-Ultra <richvniekerk@gmail.com>
2026-08-12 13:27:55 +0200

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,6 +1,7 @@
 import { handleRsvp } from "./src/handlers/rsvp.js";
 import { handleAdminLogin } from "./src/handlers/adminLogin.js";
 import { handleAdminRsvps } from "./src/handlers/adminRsvps.js";
+import { handleRsvpCount } from "./src/handlers/rsvpCount.js";
 
 export default {
   async fetch(request, env) {
@@ -8,6 +9,9 @@ export default {
 
     if (url.pathname === "/api/rsvp" && request.method === "POST") {
       return handleRsvp(request, env);
+    }
+    if (url.pathname === "/api/rsvp-count" && request.method === "GET") {
+      return handleRsvpCount(request, env);
     }
     if (url.pathname === "/api/admin/login" && request.method === "POST") {
       return handleAdminLogin(request, env);
